5c0c873d641297944328ccc25a826cf90f315ca2
ljsolano17 <ljsolano17@hotmail.com>
2021-01-25 20:08:55 -0600

    Shopping Cart

--- WebAppECartDemo/WebAppECartDemo/Controllers/ShoppingController.cs
+++ WebAppECartDemo/WebAppECartDemo/Controllers/ShoppingController.cs
@@ -3,15 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppECartDemo.Models;
+using WebAppECartDemo.ViewModel;
 
 namespace WebAppECartDemo.Controllers
 {
     public class ShoppingController : Controller
     {
+        private ECartDBEntities objECartDbEntities;
+        private List<ShoppingCartModel> listOfShoppingCartModels;
+
+
+        public ShoppingController()
+        {
+            objECartDbEntities = new ECartDBEntities();
+            listOfShoppingCartModels = new List<ShoppingCartModel>();
+        }
+
+
         // GET: Shopping
         public ActionResult Index()
         {
-            return View();
+
+            IEnumerable<ShoppingViewModel> listOfShoppingViewModel = (from objItem in objECartDbEntities.Items
+                                                                      join
+                                                                      objCate in objECartDbEntities.Categories
+                                                                      on objItem.CategoryId equals objCate.CategoryId
+                                                                      select new ShoppingViewModel()
+                                                                      {
+                                                                          ImagePath = objItem.ImagePath,
+                                                                          ItemName = objItem.ItemName,
+                                                                          Description = objItem.Description,
+                                                                          ItemPrice = objItem.ItemPrice,
+                                                                          ItemId = objItem.ItemId,
+                                                                          Category = objCate.CategoryName,
+                                                                          ItemCode = objItem.ItemCode
+                                                                      }).ToList();
+
+            return View(listOfShoppingViewModel);
         }
+        [HttpPost]
+        public JsonResult Index(string ItemId)
+        {
+
+            
+
+            ShoppingCartModel objShoppingCartModel = new ShoppingCartModel();
+            Item objItem = objECartDbEntities.Items.Single(model => model.ItemId.ToString() == ItemId);
+            if (Session["CartCounter"] != null)
+            {
+                listOfShoppingCartModels = Session["CartCounter"] as List<ShoppingCartModel>;
+            }
+            if (listOfShoppingCartModels.Any(model => model.ItemId == ItemId))
+            {
+                //Segunda vez que se agrega al carrito
+                objShoppingCartModel = listOfShoppingCartModels.Single(model => model.ItemId == ItemId);
+                objShoppingCartModel.Quantity = objShoppingCartModel.Quantity + 1;
+                objShoppingCartModel.Total = objShoppingCartModel.Quantity * objShoppingCartModel.UnitPrice;
+            }
+            else
+            {
+                //Primera vez que se agrega al carrito
+                objShoppingCartModel.ItemId = ItemId;
+                objShoppingCartModel.ImagePath = objItem.ImagePath;
+                objShoppingCartModel.ItemName = objItem.ItemName;
+                objShoppingCartModel.Quantity = 1;
+                objShoppingCartModel.Total = objItem.ItemPrice;
+                objShoppingCartModel.UnitPrice = objItem.ItemPrice;
+                listOfShoppingCartModels.Add(objShoppingCartModel);
+
+            }
+            //Llevar un contador de productos en el carrito
+            Session["CartCounter"] = listOfShoppingCartModels.Count;
+            Session["CartItem"] = listOfShoppingCartModels;
+            return Json(new { Success = true, Counter = listOfShoppingCartModels.Count },JsonRequestBehavior.AllowGet);
+
+        }
+
     }
 }
